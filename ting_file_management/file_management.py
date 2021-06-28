915a161380058ac70cc17ba4742fdb7155470ca0
CristianoNunes <cristianonunes99@gmail.com>
2021-06-28 09:42:15 -0300

    Resolved Challenge 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if ".txt" not in path_file:
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file) as file:
+            content = file.read().split("\n")
+            return content
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
