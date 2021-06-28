3bfc3ac7a43d38b965b80d5eac7f78e52921511a
CristianoNunes <cristianonunes99@gmail.com>
2021-06-28 10:06:06 -0300

    Resolved Challenge 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    contents = txt_importer(path_file)
+    out = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(contents),
+        "linhas_do_arquivo": contents
+    }
+    stop = False
+
+    for data in instance._data:
+        if(data["nome_do_arquivo"] == path_file):
+            stop = True
+
+    if not stop:
+        instance.enqueue(out)
+
+    sys.stdout.write(str(out))
 
 
 def remove(instance):
